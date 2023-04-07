d05b30497ca198033adb6accf9368ef3f914b853
Maxime Bimont <maxime.bimont.etu@univ-lille.fr>
2023-04-07 08:54:22 +0200

    modif class game

--- server/game.js
+++ server/game.js
@@ -1,9 +1,78 @@
 import Player from './player.js';
-import Map from './map.js';
+import Star from './star.js';
+import Color from './color.js';
 
-const map = new Map();
-const p1 = new Player();
+export default class Game {
+	static width = 50;
+	static height = 50;
+	nbStarMax;
+	stars = [];
+	players = [];
+	deadQueue = [];
 
-function moovePlayer(player, map) {}
+	constructor(width, height) {
+		Game.width = width;
+		Game.height = height;
+		this.nbStarMax = width * height * 0.02;
+	}
 
-function eatBubble(map, player) {}
+	generateStars() {
+		this.stars.push(
+			new Star(
+				Math.random() * Game.width - Game.width / 2,
+				Math.random() * Game.height - Game.height / 2
+			)
+		);
+	}
+
+	join(socketId, name) {
+		if (this.players.find(p => p.socketId == socketId) != undefined)
+			return false;
+		this.players.push(
+			new Player(
+				Math.random() * Game.width - Game.width / 2,
+				Math.random() * Game.height - Game.height / 2,
+				Color.randomColor(),
+				10,
+				socketId,
+				name
+			)
+		);
+		return true;
+	}
+
+	disconnect(socketId) {
+		const idx = this.players.findIndex(player => player.socketId == socketId);
+		if (idx >= 0) this.players.splice(idx, 1);
+	}
+
+	update() {
+		this.players.forEach(player => {
+			player.update();
+			for (let i = this.stars.length - 1; i >= 0; i--) {
+				if (player.canEatStar(this.stars[i])) {
+					this.stars.splice(i, 1);
+				}
+			}
+		});
+		for (let i = this.players.length - 1; i >= 0; i--) {
+			for (const other of this.players) {
+				if (
+					this.players[i].socketId != other.socketId &&
+					other.canEatStar(this.players[i])
+				) {
+					this.deadQueue.push(this.players[i].socketId);
+					this.players.splice(i, 1);
+					break;
+				}
+			}
+		}
+
+		if (this.stars.length <= this.nbStarMax) this.generateStars();
+	}
+
+	setDirection(socketId, x, y) {
+		const p = this.players.find(p => p.socketId == socketId);
+		if (p != undefined) p.setDirection(x, y);
+	}
+}
