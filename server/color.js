d2397deb6619ee90d30b3c257dd69c46c21bd783
Loïc Lecointe <loic.lecointe.etu@univ-lille.fr>
2023-04-07 09:54:33 +0200

    refactor

--- server/color.js
+++ server/color.js
@@ -1,7 +1,7 @@
 export default class Color {
 	static colors = ['white'];
 
-	static randomColor() {
-		return this.colors[Math.floor(Math.random() * this.colors.length)];
+	static getColor() {
+		return this.colors;
 	}
 }
--- server/star.js
+++ server/star.js
@@ -3,6 +3,6 @@ import Color from './color.js';
 
 export default class Start extends Cell {
 	constructor(x, y) {
-		super(x, y, Color.randomColor(), 1);
+		super(x, y, Color.getColor(), 1);
 	}
 }
